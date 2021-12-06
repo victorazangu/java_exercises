0c103f7ecefc4c42f763d3d8fb41668f88ad65aa
victor azangu <victorsazangu@gmail>
2021-12-06 14:37:26 +0300

    java exercise

---
+++ src/exercise.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class exercise {
+    public static void main(String[]args) {
+
+        //exercise 1
+        //write a java program that calculates the factorial of 10
+        int result = 1;
+
+
+        int i;
+        for (i =10;i>= 1; i--) {
+            result = result * i;
+
+        }
+
+        System.out.print(result);
+
+    }
+    }
